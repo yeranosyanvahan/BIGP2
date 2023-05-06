1aaeac5e28eccbaa035f1eb032420c57cc2e1b54
yeranosyanvahan <yeranosyanvahan@gmail.com>
2023-05-06 15:40:51 +0400

    add sample configs

--- config.py
+++
@@ -1,5 +0,0 @@
-# Configuration for the pipeline
-
-input_dir = 'C:\\Users\\aasryan\\Downloads\\DS206 Spring2023\\Week12\\python_sql\\queries'
-
-sql_server_config = 'C:\\Users\\aasryan\\Downloads\\DS206 Spring2023\\Week12\\python_sql\\sql_server_config.cfg'
