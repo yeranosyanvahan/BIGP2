153de9a6247afe2db2847f1daef9e8575066b28a
Anahit-N <anahit_navoyan@edu.aua.am>
2023-05-06 20:39:03 +0400

    ingested data

---
+++ queries/insert_into_Products.sql
@@ -0,0 +1,3 @@
+INSERT INTO {db}.{schema}.Products
+    ([ProductID], [ProductName], [SupplierID], [CategoryID], [QuantityPerUnit], [UnitPrice], [UnitsInStock], [UnitsOnOrder], [ReorderLevel], [Discontinued])
+    values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
---
+++ queries/insert_into_Suppliers.sql
@@ -0,0 +1,3 @@
+INSERT INTO {db}.{schema}.Suppliers
+    ([SupplierID], [CompanyName], [ContactName], [ContactTitle], [Address], [City], [Region], [PostalCode], [Country], [Phone], [Fax], [HomePage])
+    values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
