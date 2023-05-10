4780965fb3b54e32f86f48eaa8254caa7d2f4120
Anahit-N <anahit_navoyan@edu.aua.am>
2023-05-10 23:51:01 +0400

    DimShippers_SCD1

--- queries/update_table_dim_DimShippers_SCD1.sql
+++ queries/update_table_dim_DimShippers_SCD1.sql
@@ -1,7 +1,7 @@
-MERGE {db_dim}.{schema_dim}.{table_dim} AS DST -- destination
-USING {db_rel}.{schema_rel}.{table_rel} AS SRC -- source
-ON ( SRC.ShipperID = DST.ShipperID_NK )
-WHEN NOT MATCHED THEN -- there are IDs in the source table that are not in the destination table
+MERGE Orders_DIMENSIONAL_DW.dbo.DimShippers_SCD1 AS DST -- destination
+USING Orders_RELATIONAL_DB.dbo.Shippers AS SRC -- source
+ON (SRC.ShipperID = DST.ShipperID_NK)
+WHEN NOT MATCHED THEN
   INSERT (ShipperID_NK, CompanyName, Phone)
   VALUES (SRC.ShipperID, SRC.CompanyName, SRC.Phone)
 WHEN MATCHED AND (
