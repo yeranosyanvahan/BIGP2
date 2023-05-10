1f5a7d69493ef0efdcf6339c772219c0f41c64ba
HrachYeghiazaryan <hrachyeghiazaryan@gmail.com>
2023-05-10 23:20:53 +0400

    SCD 4 new fix

--- queries/create_table_dim_DimRegion_SCD4.sql
+++ queries/create_table_dim_DimRegion_SCD4.sql
@@ -1,12 +1,12 @@
-CREATE TABLE {db}.{schema}.DimRegion_SCD4(
-	RegionID int PRIMARY KEY,
-	Region_Key int NOT NULL 
-    FOREIGN KEY(Region_Key) references DimRegion_SCD4_History(Region_Key)
-);
-
 CREATE TABLE {db}.{schema}.DimRegion_SCD4_History(
     Region_Key int PRIMARY KEY,
     Description varchar(20) NOT NULL,
     Valid_From int NULL,
     Valid_To int NULL
 )
+
+CREATE TABLE {db}.{schema}.DimRegion_SCD4(
+	RegionID int PRIMARY KEY,
+	Region_Key int NOT NULL,
+    FOREIGN KEY(Region_Key) references DimRegion_SCD4_History(Region_Key)
+);
