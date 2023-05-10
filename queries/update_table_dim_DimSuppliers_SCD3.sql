8ffd3ae07565c4d5f0936e89d178a8e7f0d5138f
Anahit-N <anahit_navoyan@edu.aua.am>
2023-05-10 17:50:52 +0400

    DimSuppliers SCD3

--- queries/update_table_dim_DimSuppliers_SCD3.sql
+++ queries/update_table_dim_DimSuppliers_SCD3.sql
@@ -1,21 +1,26 @@
-MERGE {db_dim}.{schema_dim}.{table_dim} AS DST -- destination
-USING {db_rel}.{schema_rel}.{table_rel} AS SRC -- source
+DECLARE @Yesterday INT;
+DECLARE @Today INT;
+SET @Yesterday = CONVERT(INT, CONVERT(VARCHAR(8), DATEADD(DAY, -1, CAST(GETDATE() AS DATE)), 112));
+SET @Today = CONVERT(INT, CONVERT(VARCHAR(8), GETDATE(), 112));
+
+MERGE Orders_DIMENSIONAL_DW.dbo.DimSuppliers_SCD3 AS DST -- destination
+USING Orders_RELATIONAL_DB.dbo.Suppliers AS SRC -- source
 ON ( SRC.SupplierID = DST.SupplierID_NK )
 WHEN NOT MATCHED THEN -- there are IDs in the source table that are not in the destination table
-  INSERT (SupplierID, CompanyName,ContactName,ContactTitle,Address,City,Region,PostalCode,Country,Phone,Fax,HomePage) --There is no ValidTo
-  VALUES (SRC.SupplierID, SRC.CompanyName,SRC.ContactName,SRC.ContactTitle,SRC.Address,SRC.City,SRC.Region,SRC.PostalCode,SRC.Country,Phone,Fax,HomePage)
+  INSERT (SupplierID_NK, CompanyName,ContactName,ContactTitle,Address,City,Region,PostalCode,Country,Phone,Fax,HomePage, Creation_Date) --There is no ValidTo
+  VALUES (SRC.SupplierID, SRC.CompanyName,SRC.ContactName,SRC.ContactTitle,SRC.Address,SRC.City,SRC.Region,SRC.PostalCode,SRC.Country,Phone,Fax,HomePage, @Today)
 WHEN MATCHED AND (
-  ISNULL(DST.CompanyName, '') <> Isnull(SRC.CompanyName, ''),
-  ISNULL(DST.ContactName, '') <> Isnull(SRC.ContactName, ''),
-  ISNULL(DST.ContactTitle, '') <> Isnull(SRC.ContactTitle, ''),
-  ISNULL(DST.Address, '') <> Isnull(SRC.Address, ''),
-  ISNULL(DST.City, '') <> Isnull(SRC.City, ''),
-  ISNULL(DST.Region, '') <> Isnull(SRC.Region, ''),
-  ISNULL(DST.PostalCode, '') <> Isnull(SRC.PostalCode, ''),
-  ISNULL(DST.Country, '') <> Isnull(SRC.Country, ''),
-  ISNULL(DST.Phone, '') <> Isnull(SRC.Phone, ''),
-  ISNULL(DST.Fax, '') <> Isnull(SRC.Fax, ''),
-  ISNULL(DST.HomePage, '') <> Isnull(SRC.HomePage, ''),
+  ISNULL(DST.CompanyName, '') <> Isnull(SRC.CompanyName, '') AND
+  ISNULL(DST.ContactName, '') <> Isnull(SRC.ContactName, '') AND
+  ISNULL(DST.ContactTitle, '') <> Isnull(SRC.ContactTitle, '') AND
+  ISNULL(DST.Address, '') <> Isnull(SRC.Address, '') AND
+  ISNULL(DST.City, '') <> Isnull(SRC.City, '') AND
+  ISNULL(DST.Region, '') <> Isnull(SRC.Region, '') AND
+  ISNULL(DST.PostalCode, '') <> Isnull(SRC.PostalCode, '') AND
+  ISNULL(DST.Country, '') <> Isnull(SRC.Country, '') AND
+  ISNULL(DST.Phone, '') <> Isnull(SRC.Phone, '') AND
+  ISNULL(DST.Fax, '') <> Isnull(SRC.Fax, '') AND
+  ISNULL(DST.HomePage, '') <> Isnull(SRC.HomePage, ''))
   THEN 
     UPDATE SET  
       DST.CompanyName = SRC.CompanyName,
@@ -27,5 +32,5 @@ WHEN MATCHED AND (
           DST.Fax = SRC.Fax,
           DST.HomePage = SRC.HomePage,
           DST.Phone_Prev = (CASE WHEN DST.Phone <> SRC.Phone THEN DST.Phone ELSE DST.Phone_Prev END),
-          DST.Phone_Prev_ValidTo = (CASE WHEN DST.Phone <> SRC.Phone THEN @Yesterday ELSE DST.Phone_Prev_ValidTo END),
-          DST.Phone = SRC.Phone
+          DST.Change_Date = (CASE WHEN DST.Phone <> SRC.Phone THEN @Yesterday ELSE DST.Change_Date END),
+          DST.Phone = SRC.Phone;
