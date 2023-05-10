00189d0b6fc546f365c381f819b4ed5e8c3e3ecc
Anahit-N <anahit_navoyan@edu.aua.am>
2023-05-10 23:26:26 +0400

    updated DimEmployees_SCD1

--- queries/update_table_dim_DimEmployees_SCD1.sql
+++ queries/update_table_dim_DimEmployees_SCD1.sql
@@ -1,87 +1,69 @@
-MERGE Orders_DIMENSIONAL_DW.dbo.DimEmployees_SCD1 AS DST
-USING (SELECT m.*, r.EmployeeID_PK_SK as DimReportsTo FROM Orders_RELATIONAL_DB.dbo.Employees as m
-left join Orders_DIMENSIONAL_DW.dbo.DimEmployees_SCD1 as r
-ON r.EmployeeID_NK = m.ReportsTo) as SRC
+MERGE Orders_DIMENSIONAL_DW.dbo.DimEmployee_SCD_1 AS DST -- destination
+USING Orders_RELATIONAL_DB.dbo.Employees AS SRC -- source
 ON ( SRC.EmployeeID = DST.EmployeeID_NK )
-WHEN NOT MATCHED THEN 
-  INSERT (EmployeeID_NK, LastName, FirstName, Title, TitleOfCourtesy, BirthDate, HireDate, Address, City, Region, PostalCode, Country, HomePhone, Extension,  Notes, PhotoPath, ReportsTo)
-  VALUES (SRC.EmployeeID, SRC.LastName, SRC.FirstName, SRC.Title, SRC.TitleOfCourtesy, SRC.BirthDate, SRC.HireDate, SRC.Address, SRC.City, SRC.Region, SRC.PostalCode, SRC.Country, SRC.HomePhone, SRC.Extension, SRC.Notes, SRC.PhotoPath, SRC.DimReportsTo)
-WHEN MATCHED AND (
-  ISNULL(DST.ReportsTo, '') <> ISNULL(SRC.DimReportsTo, '') OR
-  ISNULL(DST.LastName, '') <> ISNULL(SRC.LastName, '') OR
-  ISNULL(DST.FirstName, '') <> ISNULL(SRC.FirstName, '') OR
-  ISNULL(DST.Title, '') <> ISNULL(SRC.Title, '') OR
-  ISNULL(DST.BirthDate, '') <> ISNULL(SRC.BirthDate, '') OR
-  ISNULL(DST.HireDate, '') <> ISNULL(SRC.HireDate, '') OR
-  ISNULL(DST.Address, '') <> ISNULL(SRC.Address, '') OR
-  ISNULL(DST.City, '') <> ISNULL(SRC.City, '') OR
-  ISNULL(DST.Region, '') <> ISNULL(SRC.Region, '') OR
-  ISNULL(DST.PostalCode, '') <> ISNULL(SRC.PostalCode, '') OR
-  ISNULL(DST.Country, '') <> ISNULL(SRC.Country, '') OR
-  ISNULL(DST.HomePhone, '') <> ISNULL(SRC.HomePhone, '') OR
-  ISNULL(DST.Extension, '') <> ISNULL(SRC.Extension, '') OR
-  ISNULL(DST.Notes, '') <> ISNULL(SRC.Notes, '') OR
-  ISNULL(DST.PhotoPath, '') <> ISNULL(SRC.PhotoPath, '') OR
-  ISNULL(DST.TitleOfCourtesy, '') <> ISNULL(SRC.TitleOfCourtesy, ''))
-  THEN
-    UPDATE SET
-        DST.LastName  = SRC.LastName ,
-        DST.FirstName  = SRC.FirstName ,
-        DST.Title  = SRC.Title ,
-        DST.BirthDate  = SRC.BirthDate ,
-        DST.HireDate  = SRC.HireDate ,
-        DST.Address  = SRC.Address ,
-        DST.City  = SRC.City ,
-        DST.Region  = SRC.Region ,
-        DST.PostalCode  = SRC.PostalCode ,
-        DST.Country  = SRC.Country ,
-        DST.HomePhone  = SRC.HomePhone ,
-        DST.Extension  = SRC.Extension ,
-        DST.Notes  = SRC.Notes ,
-        DST.PhotoPath  = SRC.PhotoPath,
-        DST.TitleOfCourtesy = SRC.TitleOfCourtesy,
-        DST.ReportsTo = SRC.DimReportsTo;
 
-MERGE Orders_DIMENSIONAL_DW.dbo.DimEmployees_SCD1 AS DST
-USING (SELECT m.*, r.EmployeeID_PK_SK as DimReportsTo FROM Orders_RELATIONAL_DB.dbo.Employees as m
-left join Orders_DIMENSIONAL_DW.dbo.DimEmployees_SCD1 as r
-ON r.EmployeeID_NK = m.ReportsTo) as SRC
-ON ( SRC.EmployeeID = DST.EmployeeID_NK )
-WHEN NOT MATCHED THEN 
-  INSERT (EmployeeID_NK, LastName, FirstName, Title, TitleOfCourtesy, BirthDate, HireDate, Address, City, Region, PostalCode, Country, HomePhone, Extension,  Notes, PhotoPath, ReportsTo)
-  VALUES (SRC.EmployeeID, SRC.LastName, SRC.FirstName, SRC.Title, SRC.TitleOfCourtesy, SRC.BirthDate, SRC.HireDate, SRC.Address, SRC.City, SRC.Region, SRC.PostalCode, SRC.Country, SRC.HomePhone, SRC.Extension, SRC.Notes, SRC.PhotoPath, SRC.DimReportsTo)
+/*
+MERGE Orders_DIMENSIONAL_DW.dbo.Employees AS DST
+USING (
+    SELECT
+        e.*,
+        d.EmployeeID_PK_SK AS EmployeeID
+    FROM
+        Orders_RELATIONAL_DB.dbo.Employees AS e
+    LEFT JOIN
+        Orders_DIMENSIONAL_DW.dbo.DimEmployees_SCD1 AS d
+    ON
+        e.EmployeeID = d.EmployeeID_NK
+) SRC
+ON (SRC.EmployeeID = DST.EmployeeID_NK)
+*/
+
+WHEN NOT MATCHED THEN
+    INSERT (
+        EmployeeID_NK, LastName, FirstName, Title, TitleOfCourtesy, BirthDate, HireDate, Address, City, Region,
+        PostalCode, Country, HomePhone, Extension, Photo, Notes, ReportsTo, PhotoPath
+    )
+    VALUES (
+        SRC.EmployeeID, SRC.LastName, SRC.FirstName, SRC.Title, SRC.TitleOfCourtesy, SRC.BirthDate, SRC.HireDate,
+        SRC.Address, SRC.City, SRC.Region, SRC.PostalCode, SCR.Country, SRC.HomePhone, SRC.Extension, SRC.Photo,
+        SRC.Notes, SRC.ReportsTo, SRC.PhotoPath
+    )
 WHEN MATCHED AND (
-  ISNULL(DST.ReportsTo, '') <> ISNULL(SRC.DimReportsTo, '') OR
-  ISNULL(DST.LastName, '') <> ISNULL(SRC.LastName, '') OR
-  ISNULL(DST.FirstName, '') <> ISNULL(SRC.FirstName, '') OR
-  ISNULL(DST.Title, '') <> ISNULL(SRC.Title, '') OR
-  ISNULL(DST.BirthDate, '') <> ISNULL(SRC.BirthDate, '') OR
-  ISNULL(DST.HireDate, '') <> ISNULL(SRC.HireDate, '') OR
-  ISNULL(DST.Address, '') <> ISNULL(SRC.Address, '') OR
-  ISNULL(DST.City, '') <> ISNULL(SRC.City, '') OR
-  ISNULL(DST.Region, '') <> ISNULL(SRC.Region, '') OR
-  ISNULL(DST.PostalCode, '') <> ISNULL(SRC.PostalCode, '') OR
-  ISNULL(DST.Country, '') <> ISNULL(SRC.Country, '') OR
-  ISNULL(DST.HomePhone, '') <> ISNULL(SRC.HomePhone, '') OR
-  ISNULL(DST.Extension, '') <> ISNULL(SRC.Extension, '') OR
-  ISNULL(DST.Notes, '') <> ISNULL(SRC.Notes, '') OR
-  ISNULL(DST.PhotoPath, '') <> ISNULL(SRC.PhotoPath, '') OR
-  ISNULL(DST.TitleOfCourtesy, '') <> ISNULL(SRC.TitleOfCourtesy, ''))
-  THEN
+        ISNULL(DST.EmployeeID_NK, '') <> ISNULL(SRC.EmployeeID, '') OR
+        ISNULL(DST.LastName, '') <> ISNULL(SRC.LastName, '') OR
+        ISNULL(DST.FirstName, '') <> ISNULL(SRC.FirstName, '') OR
+        ISNULL(DST.Title, '') <> ISNULL(SRC.Title, '') OR
+        ISNULL(DST.TitleOfCourtesy, '') <> ISNULL(SRC.TitleOfCourtesy, '') OR
+        ISNULL(DST.BirthDate, '') <> ISNULL(SRC.BirthDate, '') OR
+        ISNULL(DST.HireDate, '') <> ISNULL(SRC.HireDate, '') OR
+        ISNULL(DST.Address, '') <> ISNULL(SRC.Address, '') OR
+        ISNULL(DST.City, '') <> ISNULL(SRC.City, '') OR
+        ISNULL(DST.Region, '') <> ISNULL(SRC.Region, '') OR
+        ISNULL(DST.PostalCode, '') <> ISNULL(SRC.PostalCode, '') OR
+        ISNULL(DST.Country, '') <> ISNULL(SRC.Country, '') OR
+        ISNULL(DST.HomePhone, '') <> ISNULL(SRC.HomePhone, '') OR
+        ISNULL(DST.Extension, '') <> ISNULL(SRC.Extension, '') OR
+        ISNULL(DST.Photo, '') <> ISNULL(SRC.Photo, '') OR
+        ISNULL(DST.Notes, '') <> ISNULL(SRC.Notes, '') OR
+        ISNULL(DST.ReportsTo, '') <> ISNULL(SRC.ReportsTo, '') OR
+        ISNULL(DST.PhotoPath, '') <> ISNULL(SRC.PhotoPath, '')
+    )
+THEN
     UPDATE SET
-        DST.LastName  = SRC.LastName ,
-        DST.FirstName  = SRC.FirstName ,
-        DST.Title  = SRC.Title ,
-        DST.BirthDate  = SRC.BirthDate ,
-        DST.HireDate  = SRC.HireDate ,
-        DST.Address  = SRC.Address ,
-        DST.City  = SRC.City ,
-        DST.Region  = SRC.Region ,
-        DST.PostalCode  = SRC.PostalCode ,
-        DST.Country  = SRC.Country ,
-        DST.HomePhone  = SRC.HomePhone ,
-        DST.Extension  = SRC.Extension ,
-        DST.Notes  = SRC.Notes ,
-        DST.PhotoPath  = SRC.PhotoPath,
+        DST.EmployeeID_NK = SRC.EmployeeID,
+        DST.LastName = SRC.LastName,
+        DST.FirstName = SRC.FirstName,
+        DST.Title = SRC.Title,
         DST.TitleOfCourtesy = SRC.TitleOfCourtesy,
-        DST.ReportsTo = SRC.DimReportsTo;
+        DST.BirthDate = SRC.BirthDate,
+        DST.HireDate = SRC.HireDate,
+        DST.Address = SRC.Address,
+        DST.City = SRC.City,
+        DST.Region = SRC.Region,
+        DST.PostalCode = SRC.PostalCode,
+        DST.Country = SRC.Country,
+        DST.HomePhone = SRC.HomePhone,
+		DST.Extension = SRC.Extension
+
+WHEN NOT MATCHED BY SOURCE THEN
+    DELETE;
